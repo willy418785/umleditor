21ade1c8f36aecae1785b3f066f437c1a5ceb16b
Xuan Lin <willy418785@gmail.com>
2022-05-15 01:05:17 +0800

    refactor♻️: ButtonHandler change Canvas's state

--- src/main/java/edu/ncu/csie/umleditor/controller/ButtonHandler.java
+++ src/main/java/edu/ncu/csie/umleditor/controller/ButtonHandler.java
@@ -1,19 +1,24 @@
 package edu.ncu.csie.umleditor.controller;
 import edu.ncu.csie.umleditor.App;
 import edu.ncu.csie.umleditor.util.ButtonState;
+import edu.ncu.csie.umleditor.view.Canvas;
 
 import java.awt.event.*;
 
 public class ButtonHandler implements ActionListener{
     private ButtonState state;
-    public ButtonHandler(ButtonState state){
+    private Canvas canvas;
+    public ButtonHandler(Canvas canvas, ButtonState state){
         this.state = state;
+        this.canvas = canvas;
     }
 
     @Override
     public void actionPerformed(java.awt.event.ActionEvent e) {
         // change state of App if button is clicked
-        App.appState = this.state;
-        System.out.println(App.appState.getName());
+        for (MouseListener listener : canvas.getMouseListeners()) {
+            canvas.removeMouseListener(listener);
+        }
+        canvas.addMouseListener(canvas.handlerMap.get(state));
     }
 }
